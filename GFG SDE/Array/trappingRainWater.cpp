51f8004fe8d3bddbbc7ac32f01251b3495238724
toerso <toerso.mechanix@gmail.com>
2025-05-14 16:15:44 +0600

    Two Pointer Technique Added

--- GFG SDE/Array/trappingRainWater.cpp
+++ GFG SDE/Array/trappingRainWater.cpp
@@ -5,33 +5,58 @@
 #include<vector>
 using namespace std;
 
-class Solution {
-    public:
-      int maxWater(vector<int> &arr) {
-          int n = arr.size();
-          vector<int> prefixMax(n), suffixMax(n);
-          prefixMax[0] = arr[0], suffixMax[n - 1] = arr[n - 1];
+// class Solution {
+// using prefix and suffix max
+//     public:
+//       int maxWater(vector<int> &arr) {
+//           int n = arr.size();
+//           vector<int> prefixMax(n), suffixMax(n);
+//           prefixMax[0] = arr[0], suffixMax[n - 1] = arr[n - 1];
           
-          for (int i = 1; i < n; ++i) {
-              prefixMax[i] = max(prefixMax[i - 1], arr[i]);
-          }
+//           for (int i = 1; i < n; ++i) {
+//               prefixMax[i] = max(prefixMax[i - 1], arr[i]);
+//           }
           
-          for (int i = n - 2; i >= 0; i--) {
-              suffixMax[i] = max(suffixMax[i + 1], arr[i]);
-          }
+//           for (int i = n - 2; i >= 0; i--) {
+//               suffixMax[i] = max(suffixMax[i + 1], arr[i]);
+//           }
           
-          int total = 0;
+//           int total = 0;
           
-          for (int i = 0; i < n; ++i) {
-              int leftMax = prefixMax[i], rightMax = suffixMax[i];
+//           for (int i = 0; i < n; ++i) {
+//               int leftMax = prefixMax[i], rightMax = suffixMax[i];
               
-              if (arr[i] < leftMax and arr[i] < rightMax) {
-                  total += min(leftMax, rightMax) - arr[i];
-              }
-          }
+//               if (arr[i] < leftMax and arr[i] < rightMax) {
+//                   total += min(leftMax, rightMax) - arr[i];
+//               }
+//           }
           
-          return total;
-      }
+//           return total;
+//       }
+// };
+
+class Solution {
+    //using two pointer technique
+  public:
+    int maxWater(vector<int> &arr) {
+        int n = arr.size();
+        int total = 0, leftmax = 0, rightmax = 0;
+        int l = 0, r = n - 1;
+        
+        while (l < r) {
+            if (arr[l] <= arr[r]) {
+                total += max(0, leftmax - arr[l]);
+                leftmax = max(leftmax, arr[l]);
+                l += 1;
+            }else {
+                total += max(0, rightmax - arr[r]);
+                rightmax = max(rightmax, arr[r]);
+                r -= 1;
+            }
+        }
+        
+        return total;
+    }
 };
 
 int main() {
